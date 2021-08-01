148b802faadb094a4e99a6dbd3c0a1ead3f1b9ef
Scorpio28-Oficial <77907586+Scorpio28-Oficial@users.noreply.github.com>
2021-08-01 11:33:54 -0500

    Update Osint.sh

--- Osint.sh
+++ Osint.sh
@@ -16,13 +16,14 @@ function Osint {
 	clear
 sleep 0.5
 echo -e "${verde}
-${verde}╭━━━╮╭━━━╮╭━━╮${negro}╭━╮ ╭╮╭━━━━╮
-${verde}┃╭━╮┃┃╭━╮┃╰┫┣╯${negro}┃┃╰╮┃┃┃╭╮╭╮┃
-${verde}┃┃ ┃┃┃╰━━╮ ┃┃ ${negro}┃╭╮╰╯┃╰╯┃┃╰╯
-${verde}┃┃ ┃┃╰━━╮┃ ┃┃ ${negro}┃┃╰╮┃┃  ┃┃  
-${verde}┃╰━╯┃┃╰━╯┃╭┫┣╮${negro}┃┃ ┃┃┃  ┃┃  
-${verde}╰━━━╯╰━━━╯╰━━╯${negro}╰╯ ╰━╯  ╰╯  WEB
-${negro}=========${verde}(${blanco}Scorpio28${verde})${negro}=========${verde}" | pv -qL 3998
+${verde} ██████╗ ███████╗██╗${negro}███╗   ██╗████████╗
+${verde}██╔═══██╗██╔════╝██║${negro}████╗  ██║╚══██╔══╝
+${verde}██║   ██║███████╗██║${negro}██╔██╗ ██║   ██║   
+${verde}██║   ██║╚════██║██║${negro}██║╚██╗██║   ██║   
+╚${verde}██████╔╝███████║██║${negro}██║ ╚████║   ██║   
+${verde} ╚═════╝ ╚══════╝╚═╝${negro}╚═╝  ╚═══╝   ╚═╝   
+                                         WEB
+${negro}=========${verde}(${blanco}Scorpio28${verde})${negro}=========${verde}"
 }
 #
 # CÓDIGO
@@ -67,7 +68,7 @@ echo -e -n "${negro}
 └════════════════┘                      └══════════════┘
 ┃
 ┃
-└═>>> ${verde}" | pv -qL 3998
+└═>>> ${verde}"
 read -r Opcion_Osint
 
 [ "$Opcion_Osint" == "1" ]||[ "$Opcion_Osint" == "2" ]||[ "$Opcion_Osint" == "3" ]||[ "$Opcion_Osint" == "4" ]||[ "$Opcion_Osint" == "5" ]||[ "$Opcion_Osint" == "6" ]||[ "$Opcion_Osint" == "7" ]||[ "$Opcion_Osint" == "8" ]||[ "$Opcion_Osint" == "9" ]||[ "$Opcion_Osint" == "10" ]||[ "$Opcion_Osint" == "11" ]||[ "$Opcion_Osint" == "12" ]||[ "$Opcion_Osint" == "13" ]||[ "$Opcion_Osint" == "14" ]||[ "$Opcion_Osint" == "15" ]||[ "$Opcion_Osint" == "16" ]||[ "$Opcion_Osint" == "17" ]||[ "$Opcion_Osint" == "18" ]||[ "$Opcion_Osint" == "19" ]||[ "$Opcion_Osint" == "20" ] && break
